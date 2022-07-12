7ed5e3a79ee9f48b6412c36bffa94b30178d5abe
raushan kuamr <sraushan890@gmail.com>
2022-07-12 18:01:44 +0530

    typescript learning

--- iw_sessions/sprint_3/typescript-intro/src/index.js
+++ iw_sessions/sprint_3/typescript-intro/src/index.js
@@ -11,8 +11,70 @@ console.log(full_name);
 // Array
 var arr1 = [1, 2, 3, 4];
 var arrayOfString = ['masai', "school"];
+// interface person {      it isproviding a shape
+//     first_name:string,
+//     last_name:string,
+//     age?:number
+// }
 var masai = {
     first_name: "Masai",
     last_name: "School",
     age: 3
+};
+// Tuples -Arrays
+//  1. The number of element in the tuple is fixed
+// 2. the type of element are know in advance.
+// tuple is order is matter we can not change order
+var tuple;
+tuple = ['masi', 2, true];
+var city = 'Patna';
+// Enums -> a group of named constants
+var Color;
+(function (Color) {
+    Color["RED"] = "RED";
+    Color["GREEN"] = "GREEN";
+    Color["BLUE"] = "BLUE";
+})(Color || (Color = {}));
+var greenColor = Color.GREEN;
+console.log(greenColor);
+// others
+var u;
+var n;
+var a;
+// Functions 
+function sum(a, b) {
+    return a + b;
+}
+var add = function (a, b) {
+    if (b === void 0) { b = 10; }
+    return a + b;
+};
+console.log(sum(2, 2));
+console.log(add(2));
+var printName = function (_a) {
+    //    return `$(first_name)_$(last_name)`
+    // there is anothe case where there is not returning any thing
+    var first_name = _a.first_name, last_name = _a.last_name;
+    console.log("$(first_name)_$(last_name)");
+};
+var printName1 = function (_a) {
+    var first_name = _a.first_name, last_name = _a.last_name;
+    return "$(first_name)_$(last_name)";
+};
+var person1 = {
+    first_name: "masai",
+    last_name: "school",
+    age: 3
+};
+printName(person1);
+// union 
+var output;
+output = ["masai", 3];
+//intersection
+// type12=type1 & type2
+var coder = {
+    first_name: "Masai",
+    "last_name": "school",
+    age: 3,
+    skills: "coding"
 };
--- iw_sessions/sprint_3/typescript-intro/src/index.ts
+++ iw_sessions/sprint_3/typescript-intro/src/index.ts
@@ -21,19 +21,91 @@ let arr1:number[]=[1,2,3,4];
 let arrayOfString:string[]=['masai',"school"]
 
 // Object
-
+type softwareDeveloper ={
+    skills:string
+}
 //  Primitive type in TypeScript -> number string, boolean, null, undefined,symbol and bit-int
 
-// type person = {first_name:string; last_name:string;age:number}; // crating a blue print for the object
+type person = {first_name:string; last_name:string;age:number}; // crating a blue print for the object
 
-interface person {     // it isproviding a shape
-    first_name:string,
-    last_name:string,
-    age:number
-}
+// interface person {      it isproviding a shape
+//     first_name:string,
+//     last_name:string,
+//     age?:number
+// }
 const masai:person ={
     first_name:"Masai",
     last_name:"School",
     age:3
 }
 
+// Tuples -Arrays
+//  1. The number of element in the tuple is fixed
+// 2. the type of element are know in advance.
+// tuple is order is matter we can not change order
+let tuple:[string,number,boolean]
+tuple=['masi',2,true]
+
+let city:string='Patna'
+
+// Enums -> a group of named constants
+
+enum Color {
+    RED="RED",
+    GREEN="GREEN",
+    BLUE="BLUE"
+}
+let greenColor=Color.GREEN
+console.log(greenColor)
+
+// others
+let u:undefined;
+let n:null;
+let a:any
+
+// Functions 
+
+function sum(a:number, b:number):number{
+    return a+b
+}
+
+const add = (a:number,b:number=10):number =>{
+   return a+b
+}
+
+console.log(sum(2,2))
+console.log(add(2))
+
+const printName=({first_name,last_name}:person):void=>{
+//    return `$(first_name)_$(last_name)`
+   // there is anothe case where there is not returning any thing
+
+   console.log(`$(first_name)_$(last_name)`)
+}
+
+const printName1=({first_name,last_name}:person):string=>{
+    return `$(first_name)_$(last_name)`
+    
+}
+
+let person1:person={
+   first_name:"masai",
+   last_name:"school",
+   age:3
+}
+
+printName(person1)
+
+// union 
+
+let output:(number | string)[];
+output =["masai", 3]
+//intersection
+// type12=type1 & type2
+
+let coder: (person & softwareDeveloper) = {
+   first_name:"Masai",
+   "last_name":"school",
+   age:3,
+   skills:"coding"
+}
